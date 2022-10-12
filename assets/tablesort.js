6c48787d7b682881c1387190cb435ac1b5bbd52f
carrotcypher <carrotcypher@riseup.net>
2022-10-12 15:45:42 +0900

    Fixed fancyTables sorting order

--- assets/tablesort.js
+++ assets/tablesort.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
 
-  var fancyTableA = $("#sampleTableA").fancyTable({
-    sortColumn: 0,
-    pagination: true,
-    perPage: 50,
-    globalSearch: true
-  });
+    var fancyTableA = $("#sampleTableA").fancyTable({
+        sortColumn: 0,
+        pagination: true,
+        perPage: 50,
+        globalSearch: true,
+        sortOrder: 'descending'
+    });
 
-  var fancyTableB = $("#sampleTableB").fancyTable({
-    sortColumn: 0,
-    pagination: false,
-    perPage: 50,
-    globalSearch: true
-  });
+    var fancyTableB = $("#sampleTableB").fancyTable({
+        sortColumn: 0,
+        pagination: false,
+        perPage: 50,
+        globalSearch: true,
+        sortOrder: 'descending'
 
-  var fancyTableC = $("#sampleTableC").fancyTable({
-    sortColumn: 0,
-    pagination: true,
-    perPage: 6,
-    globalSearch: true
-  });
+    });
+
+    var fancyTableC = $("#sampleTableC").fancyTable({
+        sortColumn: 0,
+        pagination: true,
+        perPage: 6,
+        globalSearch: true,
+        sortOrder: 'descending'
+    });
 
 
 
